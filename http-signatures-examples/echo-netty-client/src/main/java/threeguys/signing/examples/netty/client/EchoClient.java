46b54e126d7e6e12149ffb85e98bbe85e8a57cfd
threeguys <4969340+threeguys@users.noreply.github.com>
2020-05-09 20:04:56 -0700

    Adding netty client
    Got netty client working with the spring boot server. The netty server
    should be coming soon.

---
+++ http-signatures-examples/echo-netty-client/src/main/java/threeguys/signing/examples/netty/client/EchoClient.java
@@ -0,0 +1,170 @@
+/** Any copyright is dedicated to the Public Domain.
+ * https://creativecommons.org/publicdomain/zero/1.0/ */
+package threeguys.signing.examples.netty.client;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import threeguys.http.signing.HttpSigner;
+import threeguys.http.signing.HttpSignerImpl;
+import threeguys.http.signing.Signatures;
+import threeguys.http.signing.examples.echo.client.ClientInfo;
+import threeguys.http.signing.examples.echo.client.ClientOptions;
+import threeguys.http.signing.examples.echo.client.KeyHelper;
+import threeguys.http.signing.netty.client.HttpSigningOutboundHandler;
+import threeguys.http.signing.providers.SimplePrivateKeyProvider;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.time.Clock;
+import java.util.Base64;
+
+public class EchoClient {
+
+    private static final Log log = LogFactory.getLog(EchoClient.class);
+
+    private final SimplePrivateKeyProvider privateKeyProvider;
+    private final KeyHelper keyHelper;
+    private final HttpSigningOutboundHandler signer;
+    private final ClientInfo info;
+    private final Gson gson;
+    private final String keyId;
+
+    public EchoClient(SimplePrivateKeyProvider privateKeyProvider, KeyHelper keyHelper, HttpSigningOutboundHandler signer, ClientInfo info, Gson gson, String keyId) {
+        this.privateKeyProvider = privateKeyProvider;
+        this.keyHelper = keyHelper;
+        this.signer = signer;
+        this.info = info;
+        this.gson = gson;
+        this.keyId = keyId;
+    }
+
+    private String digest(byte [] body) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        return "SHA-256=" + Base64.getEncoder().encodeToString(md.digest(body));
+    }
+
+    private HttpRequest generateRegister(ClientOptions opts, byte [] pubKey) throws NoSuchAlgorithmException {
+        HttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST,
+                            "/register?id=" + keyId + "&type=EC", Unpooled.wrappedBuffer(pubKey));
+        request.headers()
+                .set(HttpHeaderNames.HOST, opts.getHost())
+                .set(HttpHeaderNames.CONNECTION, HttpHeaderValues.CLOSE)
+                .set(HttpHeaderNames.CONTENT_TYPE, "text/pem")
+                .set(HttpHeaderNames.CONTENT_LENGTH, pubKey.length)
+                .set(HttpHeaderNames.CONTENT_ENCODING, HttpHeaderValues.IDENTITY)
+                .set("Digest", digest(pubKey));
+
+        return request;
+    }
+
+    private HttpRequest generateRequest(ClientOptions opts) throws NoSuchAlgorithmException {
+        byte [] body = info.getInfo();
+        HttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, opts.getPath(),
+                                                        Unpooled.wrappedBuffer(body));
+        request.headers()
+                .set(HttpHeaderNames.HOST, opts.getHost())
+                .set(HttpHeaderNames.CONNECTION, HttpHeaderValues.CLOSE)
+                .set(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
+                .set(HttpHeaderNames.CONTENT_LENGTH, body.length)
+                .set(HttpHeaderNames.CONTENT_ENCODING, HttpHeaderValues.IDENTITY)
+                .set("Digest", digest(body));
+
+        return request;
+    }
+
+    private void execute(Bootstrap bootstrap, ClientOptions opts, HttpRequest request) throws InterruptedException {
+        Channel ch = bootstrap.connect(opts.getHost(), opts.getPort()).sync().channel();
+        ch.writeAndFlush(request).sync();
+        ch.closeFuture().sync();
+    }
+
+    private void makeRequest(ClientOptions opts) throws InterruptedException, NoSuchAlgorithmException, IOException {
+
+        byte [] pubKey = keyHelper.loadPublicKeyPem(opts.getKeyPrefix());
+
+        EventLoopGroup workers = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap
+                    .group(workers)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline()
+                                    .addLast("log", new LoggingHandler(LogLevel.INFO))
+                                    .addLast("codec", new HttpClientCodec())
+                                    .addLast("signer", signer)
+                                    .addLast("handler", new EchoClientHandler());
+                        }
+
+                    });
+
+            HttpRequest register = generateRegister(opts, pubKey);
+            execute(bootstrap, opts, register);
+
+            HttpRequest request = generateRequest(opts);
+            execute(bootstrap, opts, request);
+
+        } finally {
+            workers.shutdownGracefully();
+        }
+    }
+
+    public void run(String... args) throws Exception {
+        ClientOptions opts = new ClientOptions(args);
+        PrivateKey privKey = keyHelper.loadPrivateKey(opts.getKeyPrefix());
+
+        // This is a hack since we may have generated the key, just set it later
+        privateKeyProvider.setKey(privKey);
+
+        makeRequest(opts);
+    }
+
+    public static void main(String [] args) throws Exception {
+        String keyId = ClientOptions.setKeyId();
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        SimplePrivateKeyProvider keyProvider = new SimplePrivateKeyProvider(null);
+
+        HttpSigner signer = new HttpSignerImpl(Clock.systemUTC(),
+                                                Signatures.DEFAULT_ALGORITHM,
+                                                keyId,
+                                                keyProvider,
+                                                new Signatures(),
+                                                30);
+
+        EchoClient client = new EchoClient(keyProvider,
+                                            new KeyHelper(),
+                                            new HttpSigningOutboundHandler(signer),
+                                            new ClientInfo(gson, keyId),
+                                            gson,
+                                            keyId);
+        client.run(args);
+    }
+
+}
---
+++ http-signatures-examples/echo-netty-client/src/main/java/threeguys/signing/examples/netty/client/EchoClientHandler.java
@@ -0,0 +1,58 @@
+/** Any copyright is dedicated to the Public Domain.
+ * https://creativecommons.org/publicdomain/zero/1.0/ */
+package threeguys.signing.examples.netty.client;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpObject;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.LastHttpContent;
+import io.netty.util.CharsetUtil;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.stream.Collectors;
+
+public class EchoClientHandler extends SimpleChannelInboundHandler<HttpObject> {
+
+    private static final Log log = LogFactory.getLog(EchoClientHandler.class);
+
+    private StringBuilder data = new StringBuilder();
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+        if (msg instanceof HttpResponse) {
+            HttpResponse response = (HttpResponse) msg;
+            log.info("Response status: " + response.status().code() + " " + response.status().reasonPhrase()
+                        + " " + response.protocolVersion().text());
+
+            String headers = response.headers().entries().stream()
+                    .map(e -> String.format("%s: \"%s\"", e.getKey(), e.getValue()))
+                    .collect(Collectors.joining("\n"));
+
+            if (headers.length() > 0) {
+                log.info("==> Begin Headers <==\n" + headers + "\n==> End Headers <==");
+            } else {
+                log.info("No headers in the response");
+            }
+        }
+
+        if (msg instanceof HttpContent) {
+            String body = ((HttpContent) msg).content().toString(CharsetUtil.UTF_8);
+            if (body.length() > 0) {
+                data.append(body);
+            }
+
+            if (msg instanceof LastHttpContent) {
+                log.info("RESPONSE: " + data.toString());
+            }
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        log.error("Exception in channel: " + ctx.name(), cause);
+    }
+
+}
